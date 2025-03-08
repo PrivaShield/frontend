8c0fcee01626b91a50fb1b4e3609c2ab9e07f716
sun <jioseon@gmail.com>
2025-03-08 23:23:23 +0900

    frontend

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -319,11 +319,9 @@ const HeroPage = () => {
                       <div className={styles.codeBlockContainer}>
                         <div className={styles.codeBlock}>
                           <div className={styles.codeContent}>{step.code}</div>
-                          <div className={styles.codeCopy}>
-                            <svg viewBox="0 0 24 24">
-                              <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
-                            </svg>
-                          </div>
+                          <svg viewBox="0 0 24 24">
+                            <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+                          </svg>
                         </div>
                       </div>
                     )}
